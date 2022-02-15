152739de7b8befa448251e4cc260f69f2bc381a0
Illuminatom <k.hernandezt@uniandes.edu.co>
2022-02-14 20:43:08 -0500

    Clase ingrediente completa
    Clase ingrediente completa

--- restaurante/src/controller/ingrediente.java
+++ restaurante/src/controller/ingrediente.java
@@ -1,5 +1,58 @@
 package controller;
 
-public class ingrediente {
+public class ingrediente 
+{	
+	// ************************************************************************
+	// Atributos
+	// ************************************************************************
 
+	/**
+	 * El nombre del ingrediente.
+	 */
+	private String nombre;
+	
+	/**
+	 * El costo adicional del ingrediente
+	 */
+	private int costoAdicional;
+	
+	// ************************************************************************
+	// Constructores
+	// ************************************************************************
+	
+	/**
+	 * Se crea el ingrediente con sus atributos.
+	 * @param elNombre El nombre del ingrediente
+	 * @param elCostoAdicional Cuanto cuesta de el ingrediente
+	 */
+	public Ingrediente(String elNombre, int elCostoAdicional)
+	{
+		this.nombre = elNombre;
+		this.costoAdicional = elCostoAdicional;
+	}
+	
+	// ************************************************************************
+	// Metodos para consultar los atributos
+	// **
+	
+	/**
+	 * Consulta el nombre del ingrediente
+	 * 
+	 * @return nombre
+	 */
+	
+	public String getNombre()
+	{
+		return nombre
+	}
+	
+	/**
+	 * Consulta el costo adicional
+	 * 
+	 * @return costoAdicional
+	 */
+	public int getcostoAdicional()
+	{
+		return costoAdicional
+	}
 }
